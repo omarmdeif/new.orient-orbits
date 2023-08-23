afa8ec8616b475debfd70666052b327221193b54
Omar <93540748+omarmdeif@users.noreply.github.com>
2023-08-23 18:18:42 +0300

    added tailwind and currently working on home page template

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('content')
+
+{{-- @include('partials._hero') --}}
+
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::get('hello', function () {
-    return view('helo');
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('contact', function () {
+    return view('contact');
+});
+
+Route::get('services', function () {
+    return view('services');
 });
 
